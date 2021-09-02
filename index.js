a989cf6b1433692c5534bc3247590b47602fe549
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2021-09-01 20:58:54 -0500

    Game functionality

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const grid = document.querySelector('.grid');
 const gridW = grid.offsetWidth;
 const gridH = grid.offsetHeight;
 
-const brickW = 110;
-const brickH = 35;
+const brickW = 100;
+const brickH = 20;
 const gap = 10;
 
 const paddleStart = [340, 480];
@@ -25,9 +25,7 @@ class Brick {
 
   constructor (x, y) {
     this.topleft = [x, y];
-    this.topRight = [x + brickW, y];
-    this.bottomLeft = [x, y + brickH];
-    this.bottomRight = [x + brickW, y + brickH];
+    this.bottomRight = [x + brickW + gap, y + brickH + gap];
   }
 
 }
@@ -83,19 +81,65 @@ function moveBall () {
 function checkCollision () {
 
   // bricks
-  
+  const bx = ballPosition[0];
+  const by = ballPosition[1];
+  let brickSet = new Set();
+  let bounced = [false, false]; // if multiple simultaneous collisons with wall, bricks 
+
+  allBricks.forEach( (brick, idx) => {
+    // vertical
+    if (bx >= brick.topleft[0] - ballD && bx <= brick.bottomRight[0]) {
+      if (by == brick.topleft[1] - ballD || by == brick.bottomRight[1]) {
+        brickSet.add(idx);
+        bounced[1] = true;
+      }
+    }
+    // horizontal
+    if (by >= brick.topleft[1] - ballD && by <= brick.bottomRight[1]) {
+      if (bx == brick.topleft[0] - ballD || bx == brick.bottomRight[0]) {
+        brickSet.add(idx);
+        bounced[0] = true;
+      }
+    }
+  });
+
+  // remove hit bricks
+  const remBricks = Array.from(document.querySelectorAll('.brick'));
+  remBricks.forEach( (remBrick, i) => {
+    if (brickSet.has(i)) {
+      remBrick.classList.remove('brick');
+    }
+  });
+  allBricks = allBricks.filter( (br, j) => !brickSet.has(j));
+  if (allBricks.length == 0) {
+    stopGame(1);
+  }
 
   // wall
   if (ballPosition[0] >= gridW - ballD || ballPosition[0] <= 0) {
-    bounceBall(1, 0);
+    bounced[0] = true;
   }
   if (ballPosition[1] <= 0) {
-    bounceBall(0, 1);
+    bounced[1] = true;
   }
+
+  // paddle
+  if (ballPosition[1] + ballD == paddlePosition[1]
+    && ballPosition[0] + ballD >= paddlePosition[0]
+    && ballPosition[0] <= paddlePosition[0] + paddleW
+  ) {
+    bounced[1] = true;
+    if (ballPosition[0] + ballD == paddlePosition[0] || ballPosition[0] == paddlePosition[0] + paddleW) {
+      bounced[0] = true;
+    }
+  }
+
+  // bounce
+  bounceBall(bounced[0] ? 1:0, bounced[1] ? 1:0);
+
+  // game over
   if (ballPosition[1] >= gridH - ballD) {
-    clearInterval(ballTimer);
-    stat.innerHTML = "Game Over";
-    document.removeEventListener('keydown', movePaddle);
+    stopGame(0);
   }
 
 }
@@ -105,12 +149,19 @@ function bounceBall (hdir, vdir) {
   ballMove[1] = vdir ? -ballMove[1] : ballMove[1];
 }
 
+// end game
+function stopGame (val) {
+  clearInterval(ballTimer);
+  document.removeEventListener('keydown', movePaddle);
+  stat.innerHTML = val ? "You Won" : "Game Over";
+}
+
 
 // bricks
 var allBricks = []
 for (let i = 0; i < 7; i++) {
   for (let j = 0; j < 2; j++) {
-    allBricks.push( new Brick(i*brickW, j*brickH) );
+    allBricks.push( new Brick(i*(brickW + gap), j*(brickH + gap) ));
   }
 }
 allBricks.forEach(drawBrick);
